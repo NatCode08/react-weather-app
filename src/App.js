d412f0da5c94e03a9e3a62fafec16229d15df9e3
NatCode08 <natortizm@gmail.com>
2022-04-08 18:08:39 +0200

    added a link to my project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p> This project was coded by Natalia Ortiz and is {""} </p>
+      <a
+        href="https://github.com/NatCode08/react-weather-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        Open-source{" "}
+      </a>
+    </div>
+  );
 }
 
 export default App;
