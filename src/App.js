696c8c4c68c42f29e0ee4169c6b54462f037f5bc
NatCode08 <natortizm@gmail.com>
2022-04-08 18:25:01 +0200

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <p> This project was coded by Natalia Ortiz and is {""} </p>
-      <a
-        href="https://github.com/NatCode08/react-weather-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {" "}
-        Open-source{" "}
-      </a>
+      <div className="container">
+        <h1> Weather App </h1>
+        <p> This project was coded by Natalia Ortiz and is {""} </p>
+        <a
+          href="https://github.com/NatCode08/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Open-source{" "}
+        </a>
+      </div>
     </div>
   );
 }
