e0f51a058ff8f9814a3ece811a5bb868fe166e60
NatCode08 <natortizm@gmail.com>
2022-04-18 23:43:54 +0200

    added a new link to my name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ function App() {
         <Weather city="madrid" />
         <footer>
           {" "}
-          This project was coded by Natalia Ortiz and is{" "}
+          This project was coded by{" "}
+          <a
+            href="https://github.com/NatCode08"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Natalia Ortiz
+          </a>{" "}
+          and is{" "}
           <a
             href="https://github.com/NatCode08/react-weather-app"
             target="_blank"
